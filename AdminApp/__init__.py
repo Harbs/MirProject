ad860bdd60358d999afee02b69d964ee8184542a
Harbs <harbs@in-tools.com>
2025-01-23 22:09:42 +0200

    Initial

---
+++ AdminApp/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__      = "Mordy"
+__credits__     = ["Mordy"]
+__license__     = "Private"
+__version__     = "0.0"
+__maintainer__  = "Mordy"
+__email__       = "mordyharbs@gmail.com"
+__status__      = "Development"
+
+
+from pyforms 			import BaseWidget
+from pyforms.controls 	import ControlText
+from pyforms.controls 	import ControlButton
+from pyforms.controls 	import ControlMatplotlib
+import pyform
